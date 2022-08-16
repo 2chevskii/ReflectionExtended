15cd8e90c9cea81fdc879f13ddc0a3f4d8e2e8e2
2CHEVSKII <prsroman3@gmail.com>
2022-08-16 17:05:49 +0300

    Refactor EnumerableExtensions

--- ReflectionExtended/Enumerable`TypeExtensions.cs
+++ ReflectionExtended/Enumerable`TypeExtensions.cs
@@ -6,79 +6,24 @@ namespace ReflectionExtended
 {
     public static partial class EnumerableExtensions
     {
-        public static bool AllAre(this IEnumerable<Type> self, Type target)
-        {
-            foreach (Type type in self)
-            {
-                if (!type.Is(target))
-                {
-                    return false;
-                }
-            }
+        public static bool AllAre(this IEnumerable<Type> self, Type target) => self.All( type => type.Is( target ) );
 
-            return true;
-        }
+        public static bool AllAre<T>(this IEnumerable<Type> self) => self.AllAre( typeof( T ) );
 
-        public static bool AllAre<T>(this IEnumerable<Type> self)
-        {
-            return AllAre(self, typeof(T));
-        }
+        public static bool AllAreExactly(this IEnumerable<Type> self, Type target) => self.All( type => type.IsExactly( target ) );
 
-        public static bool AllAreExactly(this IEnumerable<Type> self, Type target)
-        {
-            foreach (Type type in self)
-            {
-                if (!type.IsExactly(target))
-                {
-                    return false;
-                }
-            }
+        public static bool AllAreExactly<T>(this IEnumerable<Type> self) => self.AllAreExactly(typeof(T));
 
-            return true;
-        }
+        public static bool AllAreAssignableFrom(this IEnumerable<Type> self, Type source) =>
+        self.All( type => type.IsAssignableFrom( source ) );
 
-        public static bool AllAreExactly<T>(this IEnumerable<Type> self)
-        {
-            return AllAreExactly(self, typeof(T));
-        }
+        public static bool AllAreAssignableFrom<T>(this IEnumerable<Type> self) =>
+        self.AllAreAssignableFrom( typeof( T ) );
 
-        public static bool AllAreAssignableFrom(this IEnumerable<Type> self, Type source)
-        {
-            foreach (Type type in self)
-            {
-                if (!type.IsAssignableFrom(source))
-                {
-                    return false;
-                }
-            }
+        public static bool AllAreAssignableTo(this IEnumerable<Type> self, Type target) =>
+        self.All( type => type.IsAssignableTo( target ) );
 
-            return true;
-        }
-
-        public static bool AllAreAssignableFrom<T>(this IEnumerable<Type> self)
-        {
-            var source = typeof(T);
-
-            return AllAreAssignableFrom(self, source);
-        }
-
-        public static bool AllAreAssignableTo(this IEnumerable<Type> self, Type target)
-        {
-            foreach (Type type in self)
-            {
-                if (!type.IsAssignableTo(target))
-                {
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public static bool AllAreAssignableTo<T>(this IEnumerable<Type> self)
-        {
-            return AllAreAssignableTo(self, typeof(T));
-        }
+        public static bool AllAreAssignableTo<T>(this IEnumerable<Type> self) => self.AllAreAssignableTo( typeof( T ) );
 
         public static IEnumerable<Type> WithAttribute(
             this IEnumerable<Type> self,
